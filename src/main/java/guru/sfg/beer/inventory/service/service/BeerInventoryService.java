aa81fb19ebdb4fb1888ed4fe529d333a646e7be0
jeffreymzd <mzhde@hotmail.com>
2020-03-30 23:03:09 -0400

    Refactor to common model

--- src/main/java/guru/sfg/beer/inventory/service/service/BeerInventoryService.java
+++ src/main/java/guru/sfg/beer/inventory/service/service/BeerInventoryService.java
@@ -1,10 +1,10 @@
 package guru.sfg.beer.inventory.service.service;
 
-import guru.sfg.beer.common.event.NewInventoryEvent;
-import guru.sfg.beer.common.model.BeerDto;
 import guru.sfg.beer.inventory.service.config.JmsConfiguration;
 import guru.sfg.beer.inventory.service.domain.BeerInventory;
 import guru.sfg.beer.inventory.service.repositories.BeerInventoryRepository;
+import guru.sfg.brewery.model.BeerDto;
+import guru.sfg.brewery.model.event.NewInventoryEvent;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.jms.annotation.JmsListener;
