8da0e9b38b4863987d8e3a0fc32c17513f408272
jeffreymzd <mzhde@hotmail.com>
2020-03-30 23:23:22 -0400

    Refactor to common model

--- src/main/java/guru/sfg/brewery/model/BeerInventoryDto.java
+++ src/main/java/guru/sfg/brewery/model/BeerInventoryDto.java
@@ -5,6 +5,7 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.time.OffsetDateTime;
 import java.util.UUID;
 
 /**
@@ -15,7 +16,9 @@ import java.util.UUID;
 @AllArgsConstructor
 @Builder
 public class BeerInventoryDto {
-
+    private UUID id;
+    private OffsetDateTime createdDate;
+    private OffsetDateTime lastModifiedDate;
     private UUID beerId;
     private String beerUpc;
     private Integer quantityOnHand;
